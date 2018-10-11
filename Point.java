ffc4e0b1884dd94df40cdb32b4773d7a30cd9f04
Jason Zheng <zzheng3@stuy.edu>
2018-10-11 01:25:24 -0400

    fnished Point

---
+++ Point.java
@@ -0,0 +1,27 @@
+public class Point {
+	private double x;
+	private double y;
+	public Point(double X, double Y) {
+		x = X;
+		y = Y;
+	}
+	public Point(Point p) {
+		x = p.x;
+		y = p.y;
+	}
+	public double getX() {
+		return x;
+	}
+	public double getY() {
+		return y;
+	}
+	public double distanceTo(Point a) {
+		return Math.sqrt((a.getX() - getX()) * (a.getX() - getX()) + (a.getY() - getY()) * (a.getY() - getY()));
+	}
+	public double distance(Point a, Point b) {
+		return Math.sqrt((a.getX() - b.getX()) * (a.getX() - b.getX()) + (a.getY() - b.getY()) * (a.getY() - b.getY()));
+	}
+	public String toString() {
+		return "(" + getX() + ", " + getY() + ")";
+	}
+}
